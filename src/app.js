e0cbd298393042223d1170a70155bb7fda4eb3d6
ygstudio-game <ygstudio274@gmail.com>
2025-04-10 01:07:35 +0530

    adding pages setting

--- src/app.js
+++ src/app.js
@@ -157,7 +157,6 @@ app.post("/upload", upload.array("files"), async (req, res) => {
                 if (!isNaN(Number(range))) count += 1;
             }
         }
-        console.log(count);
         
         return count;
     }
@@ -175,10 +174,8 @@ app.post("/upload", upload.array("files"), async (req, res) => {
 
                 // If user specified specific pages, count them, else use total
                 const customPageCount = countPages(pagesStr);
-                console.log(customPageCount);
                 
                 pageCount = customPageCount || totalPdfPages;
-                console.log(pageCount);
             } catch (err) {
                 console.error("Error reading PDF:", err);
             }
